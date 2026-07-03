bf3efd5b780b1ed74750d0ee44b725a6513b5ce8
territoriodanorte <territoriodanorte@gmail.com>
2026-07-02 21:35:43 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
     <html lang="en" style={{ height: '100%', margin: 0 }}>
       <body className="m-0 h-full flex flex-col overflow-hidden bg-[#f8fafc] text-slate-900 font-sans" suppressHydrationWarning>
         <EditModeProvider>
-          <div className="flex flex-col h-full w-full max-w-[1024px] mx-auto bg-[#f8fafc] text-slate-900 font-sans overflow-hidden shadow-2xl relative">
+          <div className="flex flex-col h-full w-full max-w-[1024px] mx-auto bg-[#f8fafc] text-slate-900 font-sans overflow-auto shadow-2xl relative">
             {children}
             <EditModeToggle />
           </div>
